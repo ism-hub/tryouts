9d20a157bc49725014fb0b3d673aff8d806d3a86
Your Name <you@example.com>
2025-10-02 14:57:32 +0300

    f

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-echo Stop-server called, stopping nginx and our webapp..
-sudo systemctl stop nginx | true
-sudo systemctl stop webapp | true
+echo Stop-server called, stopping nginx and our webapp 2..
+
+systemctl --quiet is-active webapp && sudo systemctl stop webapp 2>&1 || true
+systemctl --quiet is-active nginx && sudo systemctl stop nginx 2>&1 || true
